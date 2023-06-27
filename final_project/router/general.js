5ebdd3d5fcd996ae1672bafb14dcb178695d263e
Pieter-devries <35712925+Pieter-devries@users.noreply.github.com>
2023-06-27 17:02:33 +0900

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,32 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    let isbn = parseInt(req.params.isbn)
+    return res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let author = req.params.author
+    Object.keys(books).forEach(key => {
+        let details = books[key];
+        if (details["author"] === author) {
+            res.send(details)
+        }
+    })
+    // for (const [book,details] of Object.entries(books)) {
+    //     // return console.log(`${book},${details["author"]}`)
+    //     if (details["author"] === author) {
+    //         return res.send(details);
+    //     } else {
+    //         return res.send("Nope")
+    //     }
+    // }
 });
 
 // Get all books based on title
